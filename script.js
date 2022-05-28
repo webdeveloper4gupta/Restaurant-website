38bf63a510e19a8f2ff640e1433c096cc4b75cc3
Sukritan Gupta <20051871@kiit.ac.in>
2022-05-28 13:08:40 +0530

    restaurtant website

---
+++ script.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+  $("#mycarousel").carousel({ interval: 200 });
+ 
+$("#carouselButton").click(function(){
+if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+  $("#mycarousel").carousel('pause');
+  $("#carouselButton").children("span").removeClass('fa-pause');
+  $("#carouselButton").children("span").addClass('fa-play');
+}
+else if ($("#carouselButton").children("span").hasClass('fa-play')){
+  $("#mycarousel").carousel('cycle');
+  $("#carouselButton").children("span").removeClass('fa-play');
+  $("#carouselButton").children("span").addClass('fa-pause');                    
+}
+});
+// jquery for modal reserve login
+$("#mahajan10").click(function(){ 
+$('#loginModal').modal('show');
+});
+// jquery for modal form
+$("#mahajan11").click(function(){
+$("#formmodal").modal('show');
+});
+});
